1e4676db9e204c6c529e2c8e3cc6b156d3f80bda
Konti48 <92818968+Konti48@users.noreply.github.com>
2023-01-29 22:58:45 +0300

    Complete final homework

--- Program.cs
+++ Program.cs
@@ -3,10 +3,40 @@
 можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При
 решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-["hello", "2", "world", ":-)", "215", "non", "null"] => ["2", ":-", "non"]
+["hello", "2", "world", ":-)", "215", "non", "null"] => ["2", ":-", "215", "non"]
 
 */
-using System.Linq;
-string[] array = new  string [] { "hello", "2", "world", ":-)", "215", "non", "null"};
+
+string[] array = new string[] { "hello", "2", "world", ":-)", "215", "non", "null" };
+int sizeToDisplay = 3;
+
+string[] GetNewArray(string[] array, int sizeToDisplay)
+{
+    int newSize = 0;
+    foreach (string key in array)
+    {
+        if (key.Length <= sizeToDisplay) newSize++;
+    }
+    string[] newArray = new string[newSize];
+    for (int i = 0, k = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= sizeToDisplay)
+        {
+            newArray[k] = array[i];
+            k++;
+        }
+    }
+
+    return newArray;
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+PrintArray(GetNewArray(array, sizeToDisplay));
 
 
